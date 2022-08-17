392c1d2e536e2546aacab0e0da8d12a36a53b89c
Kamal singh <kamalsingh200238@gmail.com>
2022-08-17 11:25:02 +0530

    Added some custom mappings

--- chadrc.lua
+++ chadrc.lua
@@ -4,8 +4,10 @@ M.ui = {
   theme = "onedark",
 }
 
+M.mappings = require "custom.mappings"
+
 M.plugins = {
-  user = require ("custom.plugins.new"),
-  -- override = require ("custom.plugins.override")
+  user = require "custom.plugins.new",
+  override = require "custom.plugins.override",
 }
 return M
---
+++ mappings.lua
@@ -0,0 +1,40 @@
+local M = {}
+
+M.basic = {
+
+  i = {
+    -- For better escape
+    ["jk"] = { "<ESC>", "escape" },
+  },
+
+  n = {
+    -- For save and quit
+    ["<leader>w"] = { "<cmd>w<cr>", "Save" },
+    ["<leader>q"] = { "<cmd>q<cr>", "Quit" },
+    ["<C-s>"] = { "<cmd>w!<cr>", "Force write" },
+    ["<C-q>"] = { "<cmd>q!<cr>", "Force quit" },
+
+    -- For no Highlighting
+    ["<leader>h"] = { "<cmd>nohlsearch<cr>", "No Highlight" },
+  },
+}
+
+M.packer = {
+  n = {
+    -- Packer
+    ["<leader>pc"] = { "<cmd>PackerCompile<cr>", "Packer Compile" },
+    ["<leader>pi"] = { "<cmd>PackerInstall<cr>", "Packer Install" },
+    ["<leader>ps"] = { "<cmd>PackerSync<cr>", "Packer Sync" },
+    ["<leader>pS"] = { "<cmd>PackerStatus<cr>", "Packer Status" },
+    ["<leader>pu"] = { "<cmd>PackerUpdate<cr>", "Packer Update" },
+  },
+}
+
+-- For overriding the nvim tree
+M.nvimtree = {
+  n = {
+    ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "   toggle nvimtree" },
+    ["<leader>o"] = { "<cmd> NvimTreeFocus <cr>", "Focus Explorer" },
+  },
+}
+return M
